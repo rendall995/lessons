030d4fa65645da72518e15c7d0af95407f50e6e6
rendall995 <56024007+rendall995@users.noreply.github.com>
2023-04-06 14:36:16 +0300

    Add files via upload

---
+++ lesson_19(2)/js/script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".swiper-body", {
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
