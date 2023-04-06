0cd248a717ab51849e102b6227c675957b49d81e
rendall995 <56024007+rendall995@users.noreply.github.com>
2023-04-06 15:03:52 +0300

    Add files via upload

---
+++ lesson_19(1)/js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper-body", {
+	slidesPerView: 1,
+	spaceBetween: 2530,
+	loop: true,
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+});
