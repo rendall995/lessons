def23fd21abea802d164f570c478c02a33493c02
rendall995 <56024007+rendall995@users.noreply.github.com>
2023-04-06 11:54:11 +0300

    Update script.js

--- lesson_19(1)/js/script.js
+++ lesson_19(1)/js/script.js
@@ -6,6 +6,10 @@ var swiper = new Swiper(".swiper-body", {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
+		autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
